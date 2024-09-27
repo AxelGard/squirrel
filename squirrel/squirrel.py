3060eaea21e506e2845742d746c2f9cafb254f06
Axel Gard <axel.gard@protonmail.com>
2024-09-27 11:10:57 +0200

    formated #

--- squirrel/squirrel.py
+++ squirrel/squirrel.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
 import sys
-import os 
+import os
 import typing
 from os import listdir
 from os.path import isfile, join
 import re
 
-def read_file(f_path:str) -> str:
-    with open(f_path, "r") as f: 
+
+def read_file(f_path: str) -> str:
+    with open(f_path, "r") as f:
         txt = f.read()
     return txt
 
-def get_all_files_in_dir(path:str) -> typing.List[str]:
+
+def get_all_files_in_dir(path: str) -> typing.List[str]:
     return [f for f in listdir(path) if isfile(join(path, f))]
 
-def write_to_file(to_file:str, text:str):
-    with open(to_file, "w") as f: 
+
+def write_to_file(to_file: str, text: str):
+    with open(to_file, "w") as f:
         f.write(text)
 
-def parse(html:str)->str:
-    pattern = r'\{\{(.*?)\}\}'
-    for word in re.findall(pattern, html): 
-        if os.path.isfile(word) and ".html" in word: 
+
+def parse(html: str) -> str:
+    pattern = r"\{\{(.*?)\}\}"
+    for word in re.findall(pattern, html):
+        if os.path.isfile(word) and ".html" in word:
             new_html = parse(read_file(word))
-            html = html.replace("{{"+ word +"}}", new_html)
+            html = html.replace("{{" + word + "}}", new_html)
     return html
 
 
-def build(dir_path:str):
+def build(dir_path: str):
     assert os.path.isdir(dir_path), "build got a none dir path as argument"
     if dir_path[-1] != "/":
         dir_path += "/"
@@ -44,5 +48,3 @@ if __name__ == "__main__":
     assert len(_argv) > 1, "Needs path argument"
     assert os.path.isdir(_argv[1]), "Needs a exsting dir as argument"
     build(_argv[1])
-
-
